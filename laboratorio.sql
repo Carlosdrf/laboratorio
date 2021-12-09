4aedcbae5e9ac5a59e6a79a0fcd361d6245bf56f
Carlos <cdavidrivas@hotmail.com>
2021-12-08 22:51:12 -0400

    sql

--- laboratorio.sql
+++ laboratorio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-12-2021 a las 03:43:04
+-- Tiempo de generación: 09-12-2021 a las 03:50:18
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.0.13
 
@@ -20,6 +20,49 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `laboratorio`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pacientes`
+--
+
+CREATE TABLE `pacientes` (
+  `idpaciente` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `apellido` varchar(20) NOT NULL,
+  `correo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `pacientes`
+--
+
+INSERT INTO `pacientes` (`idpaciente`, `nombre`, `apellido`, `correo`) VALUES
+(28470187, 'carlos', 'rivas', 'cdavidrivas@hotmail.com'),
+(29544666, 'alan', 'cañizalez', 'alandavid@gmail.com'),
+(42175444, 'diana', 'rivas', 'dianacrf@hotmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `resultados`
+--
+
+CREATE TABLE `resultados` (
+  `idresultado` int(11) NOT NULL,
+  `idsolicitud` int(11) NOT NULL,
+  `resultado` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `resultados`
+--
+
+INSERT INTO `resultados` (`idresultado`, `idsolicitud`, `resultado`) VALUES
+(1, 4, 'en espera'),
+(7, 12, 'en espera'),
+(8, 13, 'positivo');
 
 -- --------------------------------------------------------
 
@@ -58,6 +101,19 @@ DELIMITER ;
 --
 
 --
+-- Indices de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  ADD PRIMARY KEY (`idpaciente`);
+
+--
+-- Indices de la tabla `resultados`
+--
+ALTER TABLE `resultados`
+  ADD PRIMARY KEY (`idresultado`),
+  ADD UNIQUE KEY `idsolicitud` (`idsolicitud`);
+
+--
 -- Indices de la tabla `solicitud`
 --
 ALTER TABLE `solicitud`
@@ -69,6 +125,12 @@ ALTER TABLE `solicitud`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `resultados`
+--
+ALTER TABLE `resultados`
+  MODIFY `idresultado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT de la tabla `solicitud`
 --
 ALTER TABLE `solicitud`
@@ -77,6 +139,12 @@ ALTER TABLE `solicitud`
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `resultados`
+--
+ALTER TABLE `resultados`
+  ADD CONSTRAINT `resultados_ibfk_1` FOREIGN KEY (`idsolicitud`) REFERENCES `solicitud` (`idsolicitud`);
 
 --
 -- Filtros para la tabla `solicitud`
